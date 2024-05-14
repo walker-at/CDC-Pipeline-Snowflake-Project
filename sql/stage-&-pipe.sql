873577cf4e07e521f9df9aa52f3699567dc28843
Walker Atterbury <161479815+walker-at@users.noreply.github.com>
2024-05-14 15:03:29 -0400

    Create stage-&-pipe.sql

---
+++ sql/stage-&-pipe.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE STAGE SCD_DEMO.SCD2.customer_ext_stage
+    url='s3://bucket123me/stream_data'
+    credentials=(aws_key_id='AKIATCKASLYSZAOTML6W' aws_secret_key='l2ILdvtMxvzF1M6zao9iLUTZWK6qXi2dFP9INCZv');
+   
+
+CREATE OR REPLACE FILE FORMAT SCD_DEMO.SCD2.CSV
+TYPE = CSV,
+FIELD_DELIMITER = ","
+SKIP_HEADER = 1;
+
+SHOW STAGES;
+LIST @customer_ext_stage;
+
+
+CREATE OR REPLACE PIPE customer_s3_pipe
+  auto_ingest = true
+  AS
+  COPY INTO customer_raw
+  FROM @customer_ext_stage
+  FILE_FORMAT = CSV
+  ;
+
+show pipes;
+select SYSTEM$PIPE_STATUS('customer_s3_pipe');
+
+SELECT count(*) FROM customer_raw
+
+TRUNCATE  customer_raw;
